642ded7c9c96050634ad8a25de8782bc7f293c06
theodoroug13 <giorgostheodorou25@gmail.com>
2025-11-29 17:28:15 +0200

    small changes

--- include/dialog.h
+++ include/dialog.h
@@ -1,4 +1,10 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
 #include<stdlib.h>
 #include "shared_data.h"
 
+//Είσοδος proccess στον διάλογο(επιστρέφει: -1=αποτυχία ή id διαλόγου )
 int join_dialog(shared_data_t *data, int dialog_id, pid_t pid);
+
+#endif
