b0d89b8336c3421c8b85f6c83beb3ab76565c493
theodoroug13 <giorgostheodorou25@gmail.com>
2025-11-29 11:41:51 +0200

    created shared data header file

---
+++ include/shared_data.h
@@ -0,0 +1,36 @@
+
+#ifndef SHARED_DATA_H
+#define SHARED_DATA_H
+
+#include <semaphore.h>
+#include<sys/types.h>
+#define MAX_DIALOGS 32
+#define MAX_MSG_LENGHT 512
+#define MAX_MESSAGES 256
+#define MAX_PROCESSES 16
+
+typedef struct{
+    int active;
+    int dialog_id;
+    pid_t process[MAX_PROCESSES];
+}dialog_t;
+
+typedef struct{
+    int exists;
+    int msg_id;
+    int dialog_id;
+    int sender_id;
+    int readers_unseen;
+    char text[MAX_MSG_LENGHT];
+    
+}message_t;
+
+typedef struct{
+    sem_t mutex;
+    dialog_t dialogs[MAX_DIALOGS];
+    message_t messages[MAX_MESSAGES];
+    int next_dialog_id;
+    int next_msg_id;
+}shared_data_t;
+
+#endif
