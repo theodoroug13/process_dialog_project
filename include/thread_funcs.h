d75d7165d712602616170571064835938749794b
theodoroug13 <giorgostheodorou25@gmail.com>
2025-11-30 15:55:02 +0200

    thread_funcs header

---
+++ include/thread_funcs.h
@@ -0,0 +1,21 @@
+#ifndef THREAD_FUNCS_H
+#define THREAD_FUNCS_H
+
+#include"shared_data.h"
+
+typedef struct{
+    shared_data_t *data;
+    int dialog_id=-1;
+    pid_t pid;
+    
+}client_context_t;
+
+//Συνάρτηση για το sender thread
+void* sender_thread();
+
+//Συναάρτηση γιΑ το receiver thread
+void* receiver_thread(void* arg);
+
+
+
+#endif
