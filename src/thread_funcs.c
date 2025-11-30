6ca5fe65b25d157f41b93d675f1b5f9f189296f9
theodoroug13 <giorgostheodorou25@gmail.com>
2025-11-30 15:54:29 +0200

    started sender_thread

---
+++ src/thread_funcs.c
@@ -0,0 +1,46 @@
+#include"../include/thread_funcs.h"
+#include<stdio.h>
+#include<string.h>
+#include "../include/dialog.h"
+
+int running=1;
+
+void * sender_thread(void*arg){
+    client_context_t *context=(client_context_t *)arg;
+    char buffer[MAX_MSG_LENGTH];  
+    
+    
+
+    printf("[process %d (sender)] Type message (or TERMINATE)\n");
+
+    while(running){
+        if (fgets(buffer,sizeof(buffer),stdin)==NULL){
+            break;
+        }
+
+        //για να φεύγουμε το enter
+        int len=strlen(buffer);
+        if(len>0 && buffer[len-1]=='\n'){
+            buffer[len-1]='\0';
+        }
+
+        //σημαίνει έχουμε άδεια γραμμή
+        if (len==0){
+            continue;
+        }
+
+        if (send_message(context->data,context->dialog_id, context->pid, buffer)==-1){
+            fprintf(stderr,"[process %d (sender)] Failed to send message\n",context->pid);
+            running=0;
+            break;
+        }
+
+        if(strcmp("TERMINATE", buffer)==0){
+            printf("[process %d (sender)] TERMINATING exit sender\n", context->pid);
+            running=0;
+            break;
+        }
+
+    }
+    return NULL;
+}
